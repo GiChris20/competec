67bda1eeac3848d2814dd721ac9cea3269d7be52
GiChris20 <132722819+GiChris20@users.noreply.github.com>
2023-05-06 14:49:30 +0000

    Aula0

---
+++ turma-A/aula-0/Aula0.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Aula0 {
+	public static void main (String[] args) {
+		Scanner entrada = new Scanner(System.in);
+	  
+
+		/*//Passo 1 Declarando valor na variavel diretamente 
+		String nome = "Giovanna";
+		int idade = 16;
+		boolean cafe = true; 
+		
+		System.out.println("Nome: " + nome + " Idade: "  + idade + " Tomou café da manhã:" + cafe);
+
+
+		//Passo 2 Puxando variavel para adicionar valor
+		String inicial; 
+		inicial = "Giovanna"; 
+		int idadee = 16; 
+
+		System.out.println("Nome: " + inicial + " Idade: "  + idadee);
+		System.out.println(" ");
+
+
+		//Operações aritméticas - soma
+		int soma = 13 + 17; 
+
+		System.out.println("Resultado soma: " + soma);
+		System.out.println(" ");
+
+		//Operações aritméticas - divisão
+		double n1 = 8;
+		double n2 = 3; 
+		double divisao = n1 / n2; 
+
+		System.out.println("Valor: " + n1 + " Valor: " + n2);
+		System.out.println("Resultado divisão: " + divisao);
+		System.out.println(" ");
+
+		//exercicio1
+		double laranja = 12.50; 
+		double uva = 20; 
+		uva += laranja; 
+
+		System.out.println("Valor laranja: " + laranja + " Valor uva: " + uva); 
+		System.out.println("Soma dos valores das frutas: " + uva);
+		System.out.println(" ");
+
+		//exercicio2
+		String cor0 = "Amarela";
+		String cor1 = "Azul"; 
+
+		//Formas de apresentação
+		System.out.print("\nCores escritas: " + cor0 + " e " + cor1); 
+		System.out.println("Cores escritas: " + cor0 + " e " + cor1);
+		System.out.println(" ");*/
+
+		//Scanner
+		System.out.println("Digite seu nome: "); 
+		String nome = entrada.nextLine(); 
+
+		System.out.println("Digite sua idade: "); 
+		int idadeee = entrada.nextInt(); 
+
+		System.out.println("Seu nome é: " + nome + " e " + "você tem: " + idadeee + " anos."); 
+		
+
+	}
+}
